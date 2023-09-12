b9a8f8954cd987db98e9517a5c281669ae94617d
Ariel Kwak <anniekwak123@gmail.com>
2023-09-12 12:54:54 -0400

    Initial Commit

---
+++ TempConverterApp/TempConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConverterAppApp.swift
+//  TempConverterApp
+//
+//  Created by Ariel Kwak on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TempConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
